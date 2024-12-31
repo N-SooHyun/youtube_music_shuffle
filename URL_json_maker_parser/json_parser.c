2ebf816e3e7716304b99ceb0a7ec7a0d8cbc44e7
N-SooHyun <nsh4842@gmail.com>
2024-12-31 21:18:54 +0900

    base2

--- URL_json_maker_parser/json_parser.c
+++ URL_json_maker_parser/json_parser.c
@@ -2,44 +2,84 @@
 #include<stdlib.h>
 #include<Windows.h>
 #include<math.h>
-
-typedef enum { STRING, OBJECT, LIST, INTEGER, DOUB } ValueType;
-
-#define VALUESIZE 2048
-
-//전반적인 데이터의 값을 의미
-typedef struct Value {
-	ValueType type;  //0:STR, 1:OBJ, 2:LIST, 3:INT, 4:DOUB
-	union {
-		char stringValue[VALUESIZE];
-		struct Object* object;
-		struct ListValue* list;
-		int intValue;
-		double doubleValue;
-	};
-}Value;
-
-//{}객체를 의미 {key:value, key:value} Object -> Object
-typedef struct Object {
-	struct Value* key;
-	struct Value* value;
-	struct Object* next;
-}KeyValue;
-
-//[]리스트를 의미 값이 Object면 [{}, 1, []]  ListValue(Object) -> ListValue(int) -> ListValue(ListValue)
-typedef struct ListValue {
-	struct Value* value;
-	struct ListValue* next;
-}ListValue;
+#include<locale.h>
+#include "json_parse.h"
 
 
+void txt_file_read() {
+	//한글은 UTF-8이면서 2바이트 체계여서 char형식이 아닌 wchar_t형식으로 진행
+	//wchar_t 형식은 fgetc, fputc와 같은것들로 1바이트가 아닌 2바이트로 받아와야해서 fputwc, fgetwc로 가져와야함
+
+	FILE* pFile = NULL;
+	wchar_t* arr;
+	wchar_t end = '@';
+	int max = 1024;
+
+	arr = (wchar_t*)malloc(sizeof(wchar_t) * max);
+
+	fopen_s(&pFile, "../URL_api_address_file/video_details.txt", "r");
+
+	int i;
+	int j;
+	wchar_t c;
+
+	for (i = 0; ; i++) {
+		if (i > max) {
+			max *= 2;
+			arr = (wchar_t*)realloc(arr, sizeof(wchar_t)*max);
+		}
+		if ((c = fgetwc(pFile)) == end) break;
+		arr[i] = c;
+	}
+	arr[i] = '\0';
+
+	FILE* wFile = NULL;
+
+	fopen_s(&wFile, "../URL_api_address_file/test2.txt", "w");
+
+	for (j = 0; ; j++) {
+		if (arr[j] == '\0') break;
+		fputwc(arr[j], wFile);
+	}
+
+}
+
+void txt_file_json_maker() {
+	FILE* pFile = NULL;
+	wchar_t* arr;
+	wchar_t end = '@';
+	int max = 1024;
+
+	arr = (wchar_t*)malloc(sizeof(wchar_t) * max);
+
+	fopen_s(&pFile, "../URL_api_address_file/video_details.txt", "r");
+
+	int i;
+	int j;
+	wchar_t c;
+
+	for (i = 0; ; i++) {
+		if (i > max) {
+			max *= 2;
+			arr = (wchar_t*)realloc(arr, sizeof(wchar_t) * max);
+		}
+		if ((c = fgetwc(pFile)) == end) break;
+		arr[i] = c;
+	}
+	arr[i] = '\0';
+
+	//arr 안에는 json으로 바꿔야할 문자열들이 기록되어있음
+
+
+	
+
+}
 
 void json_parse_main() {
-	struct Object* root;
+	struct Object root;
+	
 
-	root = (struct Object*)malloc(sizeof(struct Object*) * 1024);
-
-	root->key;
+	
 
 	
 }
